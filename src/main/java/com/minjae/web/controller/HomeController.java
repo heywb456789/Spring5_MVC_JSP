0c006b05f0ed6914b05d6f6a4d67141ead443677
heywb456789 <heywb457@naver.com>
2024-02-13 22:02:11 +0900

    first commit

---
+++ src/main/java/com/minjae/web/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.minjae.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HomeController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		ModelAndView nv = new ModelAndView();
+		nv.addObject("data", "hello");
+		nv.setViewName("index");
+		return nv;
+	}
+
+	
+}
