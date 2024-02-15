4ecbd5f1ff45d73b409973d12d38ca5a2d7cb837
heywb456789 <heywb457@naver.com>
2024-02-15 22:05:07 +0900

    spring-jdbc 주입 & 연결정보 분리

--- src/main/java/com/minjae/web/controller/notice/ListController.java
+++ src/main/java/com/minjae/web/controller/notice/ListController.java
@@ -1,17 +1,31 @@
 package com.minjae.web.controller.notice;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import com.minjae.web.entity.Notice;
+import com.minjae.web.service.NoticeService;
+import com.minjae.web.service.jdbc.JDBCNoticeService;
+
 public class ListController implements Controller{
+
+	private NoticeService  noticeService;
+
+	public void setNoticeService(NoticeService noticeService) {
+		this.noticeService = noticeService;
+	}
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
 		ModelAndView mv = new ModelAndView("notice.list");
+		List<Notice> list = noticeService.getList(1,"TITLE","");
+		mv.addObject("list", list);
 		return mv;
 	}
 
