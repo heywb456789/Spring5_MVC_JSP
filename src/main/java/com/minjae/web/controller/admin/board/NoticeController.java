5e9a822eb70fbcc7bbff3b5955fa3adf8cb3d3a1
heywb456789 <heywb457@naver.com>
2024-02-19 22:29:58 +0900

    파일 업로드 적용

--- src/main/java/com/minjae/web/controller/admin/board/NoticeController.java
+++ src/main/java/com/minjae/web/controller/admin/board/NoticeController.java
@@ -3,6 +3,7 @@ package com.minjae.web.controller.admin.board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller("adminNoticeController")
 @RequestMapping("/admin/board/notice")
@@ -15,8 +16,12 @@ public class NoticeController {
 
     @RequestMapping("/reg")
     @ResponseBody
-    public String reg(String title, String content, String category, String[] foods){
+    public String reg(String title, String content, String category, String[] foods, MultipartFile file){
 
+        String fileName = file.getOriginalFilename();
+        long size = file.getSize();
+
+        System.out.println("fileName : "+fileName +"size : "+ size);
 
         return String.format("title : %s , content : %s , cate : %s", title, content, category);
     }
